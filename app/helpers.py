eee91848b715d7e0cd3f138b72fc376a15363768
Timmy485 <yeboahtimothy@gmail.com>
2024-02-19 21:38:10 +0000

    Set up basic RAG notebook with ingested files

--- app/helpers.py
+++ app/helpers.py
@@ -1,7 +1,16 @@
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    load_index_from_storage,
+    StorageContext,
+)
+import os
 from os import environ
 from dotenv import load_dotenv
-
 load_dotenv()
+
+OPENAI_API_KEY = environ.get("OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 def some_helper_function():
     pass
