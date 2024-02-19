b649d4fb3be95ce2bc48caa1a46a65f289ccc86c
Timmy485 <yeboahtimothy@gmail.com>
2024-02-19 22:43:27 +0000

    feat: set up ingestion pipeline api

--- app/helpers.py
+++ app/helpers.py
@@ -7,10 +7,32 @@ from llama_index.core import (
 import os
 from os import environ
 from dotenv import load_dotenv
+
 load_dotenv()
 
 OPENAI_API_KEY = environ.get("OPENAI_API_KEY")
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
-def some_helper_function():
-    pass
+
+def retrieve_pipeline(query, response_mode="tree_summarize", similarity_top_k=5):
+    # rebuild storage context
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+
+    # load index
+    index = load_index_from_storage(storage_context=storage_context)
+    query_engine = index.as_query_engine(response_mode=response_mode, similarity_top_k=similarity_top_k)
+    response = query_engine.query(query)
+    return response.response
+
+
+def create_index(directory="./storage"):
+    filename_fn = lambda filename: {"file_name": filename}
+    # automatically sets the metadata of each document according to filename_fn
+    documents = SimpleDirectoryReader(
+        input_dir="notebooks\data", 
+        file_metadata=filename_fn,
+        # input_files=["notebooks/data/01.pdf","notebooks/data/02.pdf","notebooks/data/03.pdf","notebooks/data/04.pdf","notebooks/data/05.pdf"], file_metadata=filename_fn
+    ).load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    # store it for later
+    index.storage_context.persist(persist_dir=directory)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import jsonify, request
 from app import app
-from .helpers import some_helper_function
+from .helpers import create_index
 
 @app.route('/', methods=['POST'])
 def index():
@@ -14,6 +14,22 @@ def index():
     # Prepare the response data
     response_data = {
         "message": "Welcome to SimpliMedi-Assist!",
+    }
+
+    # Return the response
+    return jsonify(status=200, response=response_data), 200
+
+@app.route('/build-index', methods=['GET'])
+def build_index():
+    if request.method != 'GET':
+        # Method not allowed
+        return jsonify(error="Method not allowed"), 405
+
+    create_index()
+
+    # Prepare the response data
+    response_data = {
+        "message": "Vector DB set up successfully!",
     }
 
     # Return the response
